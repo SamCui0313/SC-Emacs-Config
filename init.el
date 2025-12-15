8f0e064aa65d9ce4d497dd14d89d39e363937d38
SamCui0313 <78659632+SamCui0313@users.noreply.github.com>
2025-12-15 21:21:46 +0800

    Add files via upload

---
+++ init.el
@@ -0,0 +1,29 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(setq inhibit-startup-screen t)
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode 1)
+(tool-bar-mode -1)
+(setq make-backup-files nil)
+
+(use-package gruvbox-theme
+  :ensure t
+  :init
+  (load-theme 'gruvbox t)
+)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
